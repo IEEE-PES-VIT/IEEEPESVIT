51e49afb9db08e32d447a916200ccc745214e799
LAKSHAY BHARADWAJ <lbharadwaj53@gmail.com>
2023-04-28 23:27:00 +0530

    Animation added to the home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          "0%": { transform: "translateX(-70%)" },
+
+          "100%": { transform: "translateX(0%)" },
+        },
+        visIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        "slide-in": "slideIn 1s ",
+        "visible-in": "visIn 1.5s",
+      },
+    },
   },
   plugins: [],
 };
