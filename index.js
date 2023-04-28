198e00439dc13276745251f8e8c9404ea923cbd0
LAKSHAY BHARADWAJ <lbharadwaj53@gmail.com>
2023-04-28 17:24:55 +0530

    about section slider added

---
+++ index.js
@@ -0,0 +1,23 @@
+// const paras = ["para_a", "para_b", "para_c"];
+// var paraAttr = [];
+// for (i in paras) {
+//   var item = document.getElementById(paras[i]);
+//   paraAttr.push(item);
+// }
+// console.log(paraAttr);
+
+// const shuffle = () => {
+//   var i = 0;
+//   const x = setInterval(() => {
+//     paraAttr.map((para) => para.classList.add("hidden"));
+//     paraAttr[i].classList.toggle("opacity-1");
+//     //   .toggle("opacity-0");
+//     //   .toggle("hidden")
+//     i++;
+//     if (i == 3) {
+//       clearInterval(x);
+//     }
+//   }, 3000);
+// };
+
+// setInterval(shuffle, 3000);
