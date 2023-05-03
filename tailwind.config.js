299aef10bc6fdbf4ee1a6085095c18880296fc75
LAKSHAY BHARADWAJ <lbharadwaj53@gmail.com>
2023-05-03 14:40:28 +0530

    Boards layout added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,24 @@ module.exports = {
     extend: {
       keyframes: {
         slideIn: {
-          "0%": { transform: "translateX(-70%)" },
+          "0%": { transform: "translateX(-70%)", opacity: ".3" },
 
-          "100%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(0%)", opacity: "1" },
         },
         visIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
+        slideDown: {
+          "0%": { transform: "translateY(-100%)", opacity: ".3" },
+
+          "100%": { transform: "translateY(0%)", opacity: "1" },
+        },
       },
       animation: {
         "slide-in": "slideIn 1s ",
         "visible-in": "visIn 1.5s",
+        "slide-down": "slideDown 1s",
       },
     },
   },
