82e332627966c329f34abdafeba2c89a77d644ba
LAKSHAY BHARADWAJ <lbharadwaj53@gmail.com>
2023-05-24 22:32:17 +0530

    contact section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
+        visOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+
         slideDown: {
           "0%": { transform: "translateY(-100%)", opacity: ".3" },
 
@@ -21,6 +26,8 @@ module.exports = {
       animation: {
         "slide-in": "slideIn 1s ",
         "visible-in": "visIn 1.5s",
+        "visible-Dis": "visIn .55s",
+        "visible-Out": "visOut .75s",
         "slide-down": "slideDown 1s",
       },
     },
